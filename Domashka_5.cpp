7794054ed65c53c658cfcc767d3cea21c46a33d6
N-T1 <nastya.1227@yandex.ru>
2022-04-26 23:49:17 +0300

    5

---
+++ Domashka_5.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+#include "dom_5.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    run();
+}
---
+++ dom_5.h
@@ -0,0 +1,15 @@
+#ifndef DOM_5
+#define DOM_5
+#include <iostream>
+#include <string>
+using namespace std;
+
+void set();
+void pointer();
+void data();
+void print();
+void delete_ptr();
+void run();
+
+#endif
+
